5708d6905b6990564c854f5ebadf192f1647036d
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-06-06 01:10:59 -0300

    Using IApplicationBuilder

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -22,12 +23,36 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app,
                               IHostingEnvironment env,
-                              IGreeter greeter)
+                              IGreeter greeter,
+                              ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.Use(next =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                return async context =>
+                {
+                    logger.LogInformation("Request Incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path="/wp"
+            });
 
             app.Run(async (context) =>
             {
