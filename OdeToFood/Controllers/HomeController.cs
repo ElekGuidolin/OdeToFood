86a7f2549a4899f7fccce5446b54510c9ddd54de
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-06-06 05:48:49 -0300

    Very simple way to know how the view works and why it's been created for.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
         public IActionResult Index()
         {
-            var model = new Restaurant
-            {
-                Id = 1,
-                Name = "Elaine's Place"
-            };
+            var model = _restaurantData.GetAll();
+            
+            //var model = new Restaurant
+            //{
+            //    Id = 1,
+            //    Name = "Elaine's Place"
+            //};
 
             //return new ObjectResult(model);
 
