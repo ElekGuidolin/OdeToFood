97a297b16a3c85ab883397c8b92e28508b994555
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-06-06 04:12:14 -0300

    Lesson "AttributeRoutes". You can set the Controller, action, and parameters, just setting the attribute on its method, in each controller. You also have the keys to define which one is the controller, action, and parameters.

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+
+    //[Route("company/[controller]/[action]")] //It can change the route to anything you want.
+    [Route("about")]
     public class AboutController
     {
+        [Route("")] // Makes the default action.
         public string Phone()
         {
             return "+55 11 456-789-123";
         }
 
+        [Route("address")] //Makes the action when it been called.
         public string Address()
         {
             return "Brasil";
