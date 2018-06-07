d57578fef3a5939da14f78d08fe134457ecda1ef
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-06-07 17:05:21 -0300

    Lesson "POST Redirect GET Pattern". I've just change the return of the httpPost Create method to a RedirectToAction, because the way it was, if the user clicks the refresh button, the page will try to send again the values of the last post, duplicating my registers.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -55,7 +55,7 @@ namespace OdeToFood.Controllers
 
             newRestaurant = _restaurantData.Add(newRestaurant);
 
-            return View("Details", newRestaurant);
+            return RedirectToAction(nameof(Details), new { id = newRestaurant.Id });
         }
     }
 }
